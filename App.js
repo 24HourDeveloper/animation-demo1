74e448a1f52099972343df2a4388828179f4afd1
24hourdeveloper <mikeitt009@gmail.com>
2020-03-15 00:21:02 -0400

    implemented background and button fade out animation

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Image, View , Animated, Button} from 'react-native';
 
 export default function App() {
+  const [fadeOut] = useState(new Animated.Value(1))
+
+  useEffect(()=>{
+
+  },[fadeOut])
+
+  const fadeOutButton = () =>{
+    Animated.timing(fadeOut,{
+      toValue:0,
+      duration:1000
+    }).start()
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Animated.Image
+        source={require('./assets/family-bg.png')}
+        style={{width:'100%',
+        height:'100%', flex:1, justifyContent:"flex-end", opacity:fadeOut}}
+      />
+      <Animated.View style={{width:"100%", opacity:fadeOut}}>
+        <Button title="Log In" onPress={fadeOutButton}/>
+      </Animated.View>
     </View>
   );
 }
